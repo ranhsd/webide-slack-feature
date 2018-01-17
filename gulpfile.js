de2249998ab0db967c5a6abd43c31a2e13b2ed67
Ran Hassid <ran.hassid@gmail.com>
2018-01-17 09:39:33 +0200

    add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+gulp.task('default', function () {
+    gulp.src('client/**/*.js')
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// gulp.task('watch', function(){
+//     watch('src/client/**/*.js', ['js']);
+// });
